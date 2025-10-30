8de7abfc473addf8de05350007e50f1062cddf5c
kl419 <olexij.klimov@gmail.com>
2025-10-30 16:47:19 +0200

    Initial commit: 8-direction compass for micro:bit

---
+++ script.js
@@ -0,0 +1,37 @@
+let heading = 0
+basic.forever(function () 
+{
+    heading = input.compassHeading()
+    if (heading >= 337.5 || heading < 22.5) 
+    {
+        basic.showArrow(ArrowNames.North)
+    } 
+    else if (heading >= 22.5 && heading < 67.5) 
+    {
+        basic.showArrow(ArrowNames.NorthEast)
+    } 
+    else if (heading >= 67.5 && heading < 112.5) 
+    {
+        basic.showArrow(ArrowNames.East)
+    } 
+    else if (heading >= 112.5 && heading < 157.5) 
+    {
+        basic.showArrow(ArrowNames.SouthEast)
+    } 
+    else if (heading >= 157.5 && heading < 202.5) 
+    {
+        basic.showArrow(ArrowNames.South)
+    } 
+    else if (heading >= 202.5 && heading < 247.5) 
+    {
+        basic.showArrow(ArrowNames.SouthWest)
+    } 
+    else if (heading >= 247.5 && heading < 292.5) 
+    {
+        basic.showArrow(ArrowNames.West)
+    } 
+    else 
+    {
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+})
